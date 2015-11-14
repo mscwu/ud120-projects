1737f5ee0bfa3d6dd47a1b75d0331ba48e45a585
Wei Wu <mscwu@umich.edu>
2015-11-14 18:13:50 -0500

    finish mini project 1

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,22 +12,37 @@
     
 import sys
 from time import time
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+
+''' features_train and features_test are the features for the training
+    and testing datasets, respectively
+    labels_train and labels_test are the corresponding item labels
+'''
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time: ", round(time()-t1, 3), "s"
+
+print accuracy_score(labels_test, pred)
 
 
 
-#########################################################
-### your code goes here ###
 
 
-#########################################################
+
+
+
 
 
