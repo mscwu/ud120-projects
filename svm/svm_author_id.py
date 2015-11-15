e451992eb6cfd3734af3495022204efc680df4aa
Wei Wu <mscwu@umich.edu>
2015-11-15 18:28:57 -0500

    finish mini project 2

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -13,11 +13,37 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+from sklearn import svm
+
+from sklearn.metrics import accuracy_score
+
+from collections import Counter
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+clf = svm.SVC(C=10000, kernel='rbf')
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time: ", round(time()-t1, 3), "s"
+
+print accuracy_score(labels_test, pred)
+
+print pred[10], pred[26], pred[50]
+
+print Counter(pred)
+
+
+
 
 
 
