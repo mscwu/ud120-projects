d6a29c4fc20db1abb35eb1338c2c07d7db0e0e79
Wei Wu <mscwu@umich.edu>
2015-11-21 11:58:53 -0500

    get accuracy of decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -24,8 +26,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0, 3), "s"
 
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time: ", round(time()-t1, 3), "s"
 
+print accuracy_score(labels_test, pred)
+
+print "number of features in data", len(features_test[0])
 #########################################################
 
 
